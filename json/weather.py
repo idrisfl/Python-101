f8c77dac4860d6bd59c04c6c1f5696e3f9151652
Idris Lokhandwala <idris.lokhandwala@planonsoftware.com>
2021-03-04 20:45:04 +0100

    - removed api key

--- json/weather.py
+++ json/weather.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "273a4fbe7b5155b5b70434d6b290540b"
+api_key = "your_api_key"
 city = "Paris"
 url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + api_key
 
